be522bb2e1924d425770f01461fa1210ee8176fe
matheuscasadas <107257749+matheuscasadas@users.noreply.github.com>
2024-11-01 10:48:28 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação que torna sites interativos e dinâmicos, permitindo ações como cliques e animações direto no navegador'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da França é Moscou'
+)
+
+criaCartao(
+    'Programação',
+    'Defina o que é uma exceção?',
+    'Na programação orientada a objetos, uma exceção é uma notificação que interrompe a execução do programa e envia o erro para resolução ao manipulador de exceções.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz montanha?',
+    'mountain'
+)
